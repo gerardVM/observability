7781e4b264d473ea2949c36390934f06da6778b7
gerardVM <valverdegerard@gmail.com>
2024-11-30 18:22:58 +0100

    Improving integration control

--- terraform/grafana/30-aws_provider.tf
+++ terraform/grafana/30-aws_provider.tf
@@ -1,15 +1,28 @@
-resource "grafana_cloud_provider_aws_account" "provider" { # Registers an AWS account with Grafana Cloud to allow Grafana to assume a role within the AWS account.
-  for_each = local.stacks.gerardvm.aws_integration_accounts
-  stack_id = grafana_cloud_stack.stack["gerardvm"].id
+locals {
+  aws_integration_accounts = flatten([
+    for stack_key, stack_value in local.stacks : [
+      for aws_account_key, aws_account_value in stack_value.aws_integration_accounts : {
+        stack   = stack_key
+        name    = aws_account_key
+        id      = aws_account_value.id
+        regions = aws_account_value.regions
+  }]])
+}
+
+resource "grafana_cloud_provider_aws_account" "provider" {
+  for_each = { for account in local.aws_integration_accounts : account.name => account }
+
+  stack_id = grafana_cloud_stack.stack[each.value.stack].id
   role_arn = aws_iam_role.grafana_cloud.arn
-  regions  = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "eu-west-1", "eu-west-2", "eu-west-3"]
+  regions  = each.value.regions
 
   provider = grafana.cloud_provider_integration
 }
 
-resource "grafana_cloud_provider_aws_cloudwatch_scrape_job" "cw_scrape_job" { # Creates a scrape job in Grafana Cloud to scrape metrics from AWS CloudWatch.
-  for_each                = local.stacks.gerardvm.aws_integration_accounts
-  stack_id                = grafana_cloud_stack.stack["gerardvm"].id
+resource "grafana_cloud_provider_aws_cloudwatch_scrape_job" "cw_scrape_job" {
+  for_each = { for account in local.aws_integration_accounts : account.name => account }
+
+  stack_id                = grafana_cloud_stack.stack[each.value.stack].id
   name                    = each.key
   aws_account_resource_id = grafana_cloud_provider_aws_account.provider[each.key].resource_id
   export_tags             = true
